d0b5e9123c13006c21e242e29d10737900f8df80
samiul <samiulhoque67@gmail.com>
2020-11-05 11:12:15 +0600

    signout added

--- src/App.js
+++ src/App.js
@@ -34,7 +34,9 @@ function App() {
           user: null,
         });
       }
+      
     });
+    
   }, []);
 
   return (
--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,12 @@
 import React,{useState,useEffect} from 'react';
+import {Link,useHistory } from 'react-router-dom';
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 import './Nav.css';
 
 function Nav() {
+    const history=useHistory();
+    const [{  user }, dispatch] = useStateValue();
     const[show,handleShow]=useState(false);
     useEffect(() => {
         window.addEventListener("scroll",()=>{
@@ -10,9 +15,22 @@ function Nav() {
             }else handleShow(false);
         })
         return () => {
-            window.removeEventListener("scroll");
+            window.removeEventListener("scroll",()=>{
+                if(window.scrollY>100){
+                    handleShow(true);
+                }else handleShow(false);
+            });
         };
     }, []);
+    
+  
+  const handleAuthenticaton = () => {
+    if (user) {
+      auth.signOut();
+    }
+    history.push('/');
+  }
+
 
     return (
         <div className={`nav ${show && "nav__black"}`}>
@@ -21,12 +39,21 @@ function Nav() {
             src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
             alt="Netflix Logo"
             />
+        
+        <figure onClick={handleAuthenticaton}>
             <img
+            
             className="nav__avatar"
+
             src="https://i.pinimg.com/564x/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.jpg"
             alt="Netflix Logo"
-            />
             
+             
+            />
+            <figcaption className="nav__fig">Sign out</figcaption>
+            </figure>
+            
+       
             
         </div>
     )
